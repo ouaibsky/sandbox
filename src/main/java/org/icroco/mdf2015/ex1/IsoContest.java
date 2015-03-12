cf415d154e0f01142dc614cfbf428b642d1352bf
christophe <ouaibsky@gmail.com>
2015-03-12 11:37:12 +0100

    add template for mdf 2015

---
+++ src/main/java/org/icroco/mdf2015/ex1/IsoContest.java
@@ -0,0 +1,54 @@
+package org.icroco.mdf2015.ex1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
+/**
+ * imp
+ * Created by christophe on 07/03/15.
+ */
+public class IsoContest {
+    public static void main(String[] argv) throws Exception {
+        String line;
+        Scanner sc = new Scanner(System.in);
+
+        LinkedList<String> input = new LinkedList<>();
+
+        while(sc.hasNextLine())
+            input.add(sc.nextLine());
+
+        List<String> output = getSolution(input);
+
+        for (String s: output)
+            System.out.println(s);
+    }
+
+    static List<String> getSolution(LinkedList<String> aInput) {
+        List<String> output = new ArrayList<>();
+
+        int size = Integer.parseInt(aInput.removeFirst().trim()); // TODO
+
+        while(aInput.size() != 0) {
+            aInput.removeFirst();   // TODO
+
+
+        }
+
+        // TODO Fill input
+        return output;
+    }
+
+
+
+}
+
+
+class IsoContestBase {
+    public static void localEcho( String txt ) {
+        System.err.println( txt );
+    }
+}
+
